5f52712c680ec91f92cca6d5c6214f13293d6b8d
Jonathan Landicheff <jlandic@users.noreply.github.com>
2023-10-17 00:07:09 +0200

    feature: drop inside folder

--- directory-json-drop.js
+++ directory-json-drop.js
@@ -1,15 +1,80 @@
-Hooks.on('renderSidebarTab', (app, html)=>{
-  html.find('.directory-list').on('drop', async function(event){
-          event.originalEvent.preventDefault();
-          const files = event.originalEvent.dataTransfer.files;
-          if (!files[0]) return true;
-          for (let file of files) {
-            const reader = new FileReader();
-            reader.addEventListener("load", () => {
-              try { game[app.tabName].documentClass.create(JSON.parse(reader.result)); } catch (err) {console.log(err)}
-            }, false);
-            reader.readAsText(file);
-          }
-          
+const importFiles = (files, fn) => {
+  for (let file of files) {
+    const reader = new FileReader();
+    reader.addEventListener(
+      "load",
+      () => {
+        try {
+          fn(JSON.parse(reader.result));
+        } catch (err) {
+          console.log(err);
+        }
+      },
+      false
+    );
+    reader.readAsText(file);
+  }
+};
+
+const dropInFolder = (files, folderId) => {
+  importFiles(files, async (documents) => {
+    const folder = game.folders.get(folderId);
+
+    console.log(
+      documents.map((document) => ({ ...document, folder: folderId }))
+    );
+
+    await folder.documentClass.create(
+      documents.map((document) => ({ ...document, folder: folderId }))
+    );
+  });
+};
+
+const dropInRootFolder = (files, app) => {
+  importFiles(files, async (documents) => {
+    console.log(document);
+    await game[app.tabName].documentClass.create(documents);
+  });
+};
+
+Hooks.on("renderSidebarTab", (app, html) => {
+  html.find(".directory-item").on("drop", async function (event) {
+    event.originalEvent.preventDefault();
+    event.stopPropagation();
+
+    const files = event.originalEvent.dataTransfer.files;
+
+    // File is dropped on a document
+    if (event.currentTarget.attributes["data-document-id"] !== undefined) {
+      const document = game[app.tabName].get(
+        event.currentTarget.attributes["data-document-id"].value
+      );
+      console.log("Dropped onto document", document.id);
+
+      // ...inside the root folder
+      if (document.folder === null) {
+        dropInRootFolder(files, app);
+      } else {
+        dropInFolder(files, document.folder.id);
+      }
+    }
+
+    // File is dropped on a folder
+    else if (event.currentTarget.attributes["data-folder-id"] !== undefined) {
+      console.log(
+        "Dropped onto folder",
+        event.currentTarget.attributes["data-folder-id"]
+      );
+      dropInFolder(
+        files,
+        event.currentTarget.attributes["data-folder-id"].value
+      );
+    }
+  });
+
+  html.find(".directory-list").on("drop", async function (event) {
+    event.originalEvent.preventDefault();
+
+    dropInRootFolder(event.originalEvent.dataTransfer.files, app);
   });
 });
